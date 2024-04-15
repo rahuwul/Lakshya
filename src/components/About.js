bbd565d0a1bc085ad22ce8021bd903419455f775
rahuwul <rahuwul@gmail.com>
2024-04-16 00:09:39 +0530

    updated about.js

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ export default function About() {
           <div className='bg-[#9525e8] w-48 h-1'></div>
         </div>
         <div className='' >
-          <h1 id='abouttext' className='text-right'>Lakshya started as a project during a hackathon, where I took the lead in crafting the entire frontend experience from scratch. From whipping up designs in Figma to coding them into a proper webpage with <b>ReactJS</b> and <b>TailwindCSS</b>, it was a fun! In this project, I fine-tuned my frontend skills and tackled everything from React Hooks to the fine art of centering a div using Tailwind :D . Looking forward to design more! </h1>
+          <h1 id='abouttext' className='text-right'>Lakshya started as a project during a hackathon, where I took the lead in crafting the entire frontend experience from scratch. From whipping up designs in Figma to coding them into a proper webpage with <b>ReactJS</b> and <b>TailwindCSS</b>, it was fun learning experience! In this project, I fine-tuned my frontend skills and tackled everything from React Hooks to the "fine art of centering a div" using Tailwind :D . Looking forward to design more! </h1>
           </div>
           <div className='flex justify-end py-2 '>
           <div className='bg-[#9525e8] w-24 h-1'></div>
@@ -24,9 +24,10 @@ export default function About() {
           <div className='bg-[#9525e8] w-12 h-1'></div>
         </div>
         <div className='flex flex-row  justify-center' >
-        <div id='linkbox' className='flex flex-row  justify-center border-black border-2 w-[9.5rem] rounded-lg bg-[#9525e8]'>
+        <div id='linkbox' className='flex flex-row  justify-center border-black border-2 w-[13rem] rounded-lg bg-[#9525e8]'>
           <div className='relative border-2 cursor-pointer  border-black bg-white hover:-translate-y-3 transition-all duration-300  p-2 rounded-tl-lg rounded-bl-lg'><a href='https://www.linkedin.com/in/rahul-verma-568ab9217/' target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin fa-2xl" style={{color:"#9525e8"}}></i></a></div>
           <div className='relative border-2 cursor-pointer  border-black bg-white hover:-translate-y-3 transition-all duration-300  p-2 '><a href='https://twitter.com/rahuwul' target="_blank" rel="noreferrer" ><i class="fa-brands fa-x-twitter fa-2xl" style={{color:"#9525e8"}}></i></a></div>
+          <div className='relative border-2 cursor-pointer  border-black bg-white hover:-translate-y-3 transition-all duration-300  p-2 '><a href='https://github.com/rahuwul' target="_blank" rel="noreferrer" ><i class="fa-brands fa-github fa-2xl" style={{color:"#9525e8"}}></i></a></div>
           <div className='relative border-2 cursor-pointer  border-black bg-white hover:-translate-y-3 transition-all duration-300  p-2 rounded-tr-lg rounded-br-lg'><a href='https://www.behance.net/rahul-designs' target="_blank" rel="noreferrer" ><i class="fa-brands fa-behance fa-2xl" style={{color:"#9525e8"}}></i></a></div>
         </div>
         </div>
